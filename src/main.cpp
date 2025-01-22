2bd4b747c51d103a3f0371628408328323d8cafd
David Spörri <davepermen@hotmail.com>
2025-01-23 00:28:52 +0100

    there we go. have to rewrite the web request with a modenr library and use the actual config values, but the button works.

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <WiFiSettings.h>
 #include <LittleFS.h>
 #include "helpers.h"
+#include "config.h"
 
 DoubleResetDetector drd(10, 0);
 
 void setup() {
-  Serial.begin(9600);
+  system_update_cpu_freq(160);
+  Serial.begin(115200);
   Serial.println("booting up conesoft-web-button");
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(BUTTON1, INPUT_PULLUP);
+  pinMode(BUTTON2, INPUT_PULLUP);
   LittleFS.begin();
 
   WiFiSettings.hostname = "csft-btn-";
@@ -22,11 +26,51 @@ void setup() {
   WiFiSettings.connect();
 }
 
+void handlePressed(int button);
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(250);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(250);
+  handlePressed(BUTTON1);
+  handlePressed(BUTTON2);
+  setLed(false);
 
   drd.loop();
+  delay(10);
+}
+
+void makeWebRequest(int button) {
+  WiFiClient client;
+  int retries = 5;
+  while(!!!client.connect("192.168.1.2", 17184) && (retries-- > 0)) {
+  }
+  if(!!!client.connected()) {
+    error(2);
+  }
+  client.print(String("GET ") + "/device/action" + 
+                  " HTTP/1.1\r\n" +
+                  "Host: " + "192.168.1.2" + "\r\n" + 
+                  "User-Agent: Conesoft-Web-Button\r\n" +
+                  "Conesoft-Web-Button-Id: " + WiFi.macAddress() + "::" + button + "\r\n" +
+                  "Connection: close\r\n\r\n");
+                  
+  int timeout = 5 * 10; // 5 seconds             
+  while(!!!client.available() && (timeout-- > 0)) {
+    delay(100);
+  }
+  if(!!!client.available()) {
+    error(3);
+  }
+  while(client.available()){
+    client.read();
+  }
+  client.stop();
+}
+
+void handlePressed(int button) {
+  if(isPressed(button)) {
+    setLed(true);
+    makeWebRequest(button);
+  }
+  while(isPressed(button)) {
+    delay(10);
+  }
 }
